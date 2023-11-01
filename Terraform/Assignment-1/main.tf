cfaa904e8e5ed8f38238b381d22c079566bbcd26
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2023-11-01 21:49:28 +0530

    Add Terraform main.tf, provider.tf and variable.tf

---
+++ Terraform/Assignment-1/main.tf
@@ -0,0 +1,50 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.name_prefix}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name_prefix}-igw"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.16.0/20"
+
+  tags = {
+    Name         = "${var.name_prefix}-public-subnet-1"
+    connectivity = "public"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
